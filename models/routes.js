30754efcfdbb83bdf51b357ff67cd02003d079a7
Ali Hussnain <20042909@mydbs.ie>
2024-11-25 11:32:13 +0000

    create

---
+++ models/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const CurrencyPair = require('../models/CurrencyPair');
+
+// Create
+router.post('/add', async (req, res) => {
+  try {
+    const { baseCurrency, targetCurrency, exchangeRate } = req.body;
+    const newPair = new CurrencyPair({ baseCurrency, targetCurrency, exchangeRate });
+    await newPair.save();
+    res.json({ message: 'Currency pair added successfully!', data: newPair });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+
+module.exports = router;
