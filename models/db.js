ec6e5e6ab60b37a0d5f127e9f5bf9cd649fa878c
Ali Hussnain <20042909@mydbs.ie>
2024-11-27 17:10:51 +0000

    m

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
-mongodb+srv;//Ali:Nexus143@cluster0.jm7tn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 
+const uri = "mongodb+srv://Ali:Nexus143@cluster0.jm7tn.mongodb.net/myDatabase?retryWrites=true&w=majority";
 
