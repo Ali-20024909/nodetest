83a36358ad7c4cd69af2c98b3e800a103665407d
Ali Hussnain <20042909@mydbs.ie>
2024-11-27 17:45:12 +0000

    .

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CurrencyPair = require('../models/CurrencyPair');
+const CurrencyPair = require('./CurrencyPair');
 
 // Create
 router.post('/add', async (req, res) => {
