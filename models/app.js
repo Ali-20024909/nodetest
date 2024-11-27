2823ed98c96f8d5d297f0251e372adc81b813d46
Ali Hussnain <20042909@mydbs.ie>
2024-11-27 14:16:26 +0000

    conectiom

--- models/app.js
+++ models/app.js
@@ -5,3 +5,8 @@ const uri = 'mongodb+srv://Ali:Nexus143.@clustername.mongodb.net/myFirstDatabase
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected successfully!'))
   .catch(err => console.error('Database connection error:', err));
+  const connectToDatabase = require('./db'); // Import the connection logic
+
+  // Call the connection function to test the connection
+  connectToDatabase().catch(console.error);
+  
