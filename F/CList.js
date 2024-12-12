58dd93bb9f0865008be775b54c1467fcd8e5de7a
Ali Hussnain <20042909@mydbs.ie>
2024-12-12 00:13:40 +0000

    amount

--- F/CList.js
+++ F/CList.js
@@ -160,3 +160,12 @@ function renderClients(clientsToRender) {
     
     elements.tableView.innerHTML = tableHTML;
 }
+
+function formatCurrency(amount) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    }).format(amount);
+}
