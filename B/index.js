e7fe83bbcc8a92a7c03598e6c4f8a24a5893225b
Ali Hussnain <20042909@mydbs.ie>
2024-12-12 01:51:51 +0000

    middle

--- B/index.js
+++ B/index.js
@@ -9,3 +9,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET || 'Moeez+Butt=MoeezButt';
 
+
+// Middleware
+const corsOptions = {
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // Live Server URLs
+    credentials: true
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
